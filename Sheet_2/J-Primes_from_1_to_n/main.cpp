479a22831e48b3b6457a6934aec007f68bf72a6c
AhmedFatthy1040 <eltonsy1040@outlook.sa>
2022-10-13 08:09:05 +0200

    Print all Prime Numbers Between 1 and N

--- Sheet_2/J-Primes_from_1_to_n/main.cpp
+++ Sheet_2/J-Primes_from_1_to_n/main.cpp
@@ -1,10 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+void check(int num) {
+    int n = num / 2;
+    bool flag = false;
+    for(int i = 2; i <= n; i++) {
+        if(num % i == 0) {
+            flag = true;
+            break;
+        }
+    }
+    if(flag == false)
+        cout << num << " ";
+}
 int main() {
     int n;
     cin >> n;
-    for(int i = 0; i < n; i++) {
-        for(int )
+    for(int i = 2; i < n; i++) {
+        check(i);
     }
     return 0;
 }
